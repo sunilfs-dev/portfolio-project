1f21ec9478ad260b32ccf4427e878e64289f2ccd
sunilFS-dev <sunielsethy@gmail.com>
2025-07-12 19:18:18 +0530

    aaded port

--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ app.get('/', (req, res) => {
   res.send('Backend is running!');
 });
 
+app.listen(process.env.PORT || 5000);
+
 app.listen(5000, () => {
   console.log('Server listening on port 5000');
 });
